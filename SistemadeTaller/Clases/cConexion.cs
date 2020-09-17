addd0389abc6a616df6a6a24f8464a7523d49fc7
pablomartinzabala2 <pablomartinzabala2@gmail.com>
2020-09-17 15:55:28 -0300

    ff
    ff

--- SistemadeTaller/Clases/cConexion.cs
+++ SistemadeTaller/Clases/cConexion.cs
@@ -10,13 +10,13 @@ namespace SistemadeTaller.Clases
         public static  string Cadenacon()
         {
             //taller
-             string cadena = "Data Source=DESKTOP-QKECIIE;Initial Catalog=TALLER;Integrated Security=True";
+           //  string cadena = "Data Source=DESKTOP-QKECIIE;Initial Catalog=TALLER;Integrated Security=True";
             //copiataller
           //   string cadena = "Data Source=DESKTOP-QKECIIE;Initial Catalog=copiataller;Integrated Security=True";
             //    string cadena = "Data Source=LOCALHOST\\SQLEXPRESS;Initial Catalog=TALLER;Integrated Security=True";
             //     string cadena = "Data Source=DESKTOP-QKECIIE;Initial Catalog=TALLER;Integrated Security=True";
             //taller
-          //  string cadena = "Data Source=AGENCIA2\\SQLEXPRESS;Initial Catalog=TALLER;Integrated Security=True";
+            string cadena = "Data Source=AGENCIA2\\SQLEXPRESS;Initial Catalog=TALLER;Integrated Security=True";
             return cadena ;
         }
     }
